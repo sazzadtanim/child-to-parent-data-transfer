9f154ebf385af52bdf71cb8ccf854e1e34d2dc4c
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-07 12:13:04 +0600

    prettier

--- components/ChildComponent.jsx
+++ components/ChildComponent.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
 export default function ChildComponent({ transferData }) {
   return (
     <div>
       <h3>CHILD COMPONENT STARTS</h3>
       <input
-        type="text"
-        placeholder="any data on child"
-        onChange={(e) => transferData(e.target.value)}
+        type='text'
+        placeholder='any data on child'
+        onChange={e => transferData(e.target.value)}
       />
       <h3>CHILD COMPONENT END</h3>
     </div>
-  );
+  )
 }
 
 //In child component, input of the transferData function is the output of the transferData function in parent componet
