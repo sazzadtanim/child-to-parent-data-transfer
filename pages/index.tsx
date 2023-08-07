7eb270277d3908a53bdaa9961a5d08cf4f541bf9
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-07 12:10:29 +0600

    added tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import ChildComponent from "../components/ChildComponent"
 import { useState } from "react";
+import ChildComponent from "../components/ChildComponent";
 
 export default function Home() {
   const [rcvChildData, setChildData] = useState("");
   return (
-    <>
+    <div className="prose">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1>Import data from child component to Parent Component</h1>
-      <ChildComponent transferData={(childData:string) => setChildData(childData)} />
+      <ChildComponent
+        transferData={(childData: string) => setChildData(childData)}
+      />
 
       <p> Child data received in Parent: {rcvChildData}</p>
-    </>
+    </div>
   );
 }
-
